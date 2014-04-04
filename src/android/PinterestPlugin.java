6c388a321fa3d31ad2f78acd28dea193bf7a8040
Yewon Lee <ywplee@gmail.com>
2014-04-03 17:12:57 -0700

    android support

--- src/android/PinterestPlugin.java
+++ src/android/PinterestPlugin.java
@@ -17,68 +17,81 @@ public class PinterestPlugin extends CordovaPlugin {
 	public static final String TAG = "PinterestPlugin";
 	public static final String INIT = "initPinterest";
 	public static final String PIN = "pin";
-	public static final String AVAILABLE = "isPinterestInstalled";
 
 	/**
 	 * Gets the application context from cordova's main activity.
 	 * @return the application context
 	 */
 	private Context getApplicationContext() {
-		return this.cordova.getActivity().getApplicationContext();
+		return this.webView.getContext();
 	}
 	
 	@Override
-    public boolean execute(String action, CordovaArgs args, final CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, CordovaArgs args, 
+    		final CallbackContext callbackContext) throws JSONException {
     	boolean result = false;
-		String sourceUrl, imageUrl, description;
-		PinIt pinIt;
-		
+		final String sourceUrl, imageUrl, description;
+		final PinItListener _listener = new PinItListener() {
+	        @Override
+	        public void onStart() {
+	            super.onStart();
+	            Log.i(TAG, "PinItListener.onStart");
+	        }
+
+	        @Override
+	        public void onComplete(boolean completed) {
+	            super.onComplete(completed);
+	            Log.i(TAG, "PinItListener.onComplete");
+	            if (completed) {
+					callbackContext.success();
+	            } else {
+					callbackContext.error("Pinterest couldn't pint it.");
+	            }
+	        }
+
+	        @Override
+	        public void onException(Exception e) {
+	            super.onException(e);
+	            Log.e(TAG, e.getMessage());
+				callbackContext.error(e.getMessage());
+	        }
+
+	    };
 		Log.v(TAG, "execute: action=" + action);
 		if (INIT.equals(action)) {
-			Log.v(TAG, "Init: client ID=" + args.toString());
-			PinItButton.setPartnerId(args.getString(0));
+			Log.v(TAG, "Init: client ID=" + args.getString(0));
+			final String id = args.getString(0);
+			cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+        			PinItButton.setPartnerId(id);
+        			PinItButton.setDebugMode(true);
+                    callbackContext.success(); // Thread-safe.
+                }
+            });
 			result = true;
-			callbackContext.success();
 		}
 		else if (PIN.equals(action)) {
 			sourceUrl = args.getString(0);
 			imageUrl = args.getString(1);
 			description = args.getString(2);
-			Log.v(TAG, "PinIt: source=" + sourceUrl + ", imageUrl: " + imageUrl + 
-				       ", description: " + description);
+			Log.v(TAG, "PinIt: source=" + sourceUrl + ", imageUrl: " + 
+					imageUrl + ", description: " + description);
+			cordova.getThreadPool().execute(new Runnable() {
+                public void run() {
+                	final PinIt pinIt = new PinIt();
 
-			pinIt = new PinIt();
-			pinIt.setUrl(sourceUrl);
-			pinIt.setImageUrl(imageUrl);
-			pinIt.setDescription(description);
-			pinIt.setListener(_listener);
-			pinIt.doPinIt(getApplicationContext());
-			callbackContext.success();
+        			pinIt.setUrl(sourceUrl);
+        			pinIt.setImageUrl(imageUrl);
+        			pinIt.setDescription(description);
+        			pinIt.setListener(_listener);
+        			pinIt.doPinIt(getApplicationContext());
+                }
+            });
 			result = true;
 		}
 		return result;
 		
 	}
-	PinItListener _listener = new PinItListener() {
 
-        @Override
-        public void onStart() {
-            super.onStart();
-            Log.i(TAG, "PinItListener.onStart");
-        }
-
-        @Override
-        public void onComplete(boolean completed) {
-            super.onComplete(completed);
-            Log.i(TAG, "PinItListener.onComplete");
-        }
-
-        @Override
-        public void onException(Exception e) {
-            super.onException(e);
-            Log.e(TAG, e.getMessage());
-        }
-
-    };
 
 }
