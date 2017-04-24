4d0a7de38618a20b38fecd010587283a537c61e0
njzk2 <masperos+github@gmail.com>
2017-04-24 14:30:18 -0400

    Update ios pinterest framework and plugin

--- src/ios/PinterestPlugin.m
+++ src/ios/PinterestPlugin.m
@@ -3,55 +3,38 @@
 
 #import "PinterestPlugin.h"
 
-@implementation PinterestPlugin
+#import "PDKClient.h"
+#import "PDKPin.h"
 
-@synthesize pinterest;
-@synthesize clientId;
+@implementation PinterestPlugin
 
 - (void) initPinterest:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-
-    self.clientId = [[NSString alloc] initWithString:[command.arguments objectAtIndex:0]];
-    [clientId performSelector:NSSelectorFromString(@"retain")];
-    self.pinterest = [[Pinterest alloc] initWithClientId:self.clientId urlSchemeSuffix:@"prod"];
-    if (self.pinterest != nil) {
-        NSLog(@"Pinterest Plugin initalized with clientID: %@", self.clientId);
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-
-    } else{
-        NSLog(@"Pinterest Plugin failed to initalize.");
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
-    }
-     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    NSString* appId = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"PinterestAppId"];
+    [PDKClient configureSharedInstanceWithAppId:appId];
 }
 
 - (void) pin:(CDVInvokedUrlCommand*)command {
     NSURL *sourceURL = [NSURL URLWithString:[command.arguments objectAtIndex:0]];
     NSURL *imageURL = [NSURL URLWithString:[command.arguments objectAtIndex:1]];
-	NSString* description =[NSString stringWithString:[command.arguments objectAtIndex:2]];
-    CDVPluginResult* pluginResult = nil;
+    NSString* description =[NSString stringWithString:[command.arguments objectAtIndex:2]];
 
-    if ([pinterest canPinWithSDK]) {
-        [pinterest createPinWithImageURL:imageURL
-                               sourceURL:sourceURL
-                             description:description];
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Pinterest not available"];
-    }
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    
-}
-- (void)canPinWithSDK:(CDVInvokedUrlCommand*)command {
-	CDVPluginResult* pluginResult = nil;
-    
-	if ([pinterest canPinWithSDK]) {
-		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-	} else {
-		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Can't pin with SDK"];
-	}
-	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    __weak PinterestPlugin *weakSelf = self;
+
+    [PDKPin pinWithImageURL:imageURL
+                       link:sourceURL
+         suggestedBoardName:@""
+                       note:description
+                withSuccess:^ {
+                  CDVPluginResult* pluginResult = nil;
+                  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+                  [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                 }
+                 andFailure:^(NSError *error) {
+                   CDVPluginResult* pluginResult = nil;
+                   pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+                   [weakSelf.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                 }];
 }
 
 @end
--- www/pinterest.js
+++ www/pinterest.js
@@ -1,14 +1,11 @@
 function PinterestPlugin() {
 	// Does nothing
 }
-PinterestPlugin.prototype.init = function(clientID, successCallback, errorCallback) {
-	cordova.exec(successCallback, errorCallback, "PinterestPlugin", "initPinterest", [clientID]);
+PinterestPlugin.prototype.init = function(successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, "PinterestPlugin", "initPinterest", []);
 };
 
 PinterestPlugin.prototype.pin = function(sourceURL, imageURL, description, successCallback, errorCallback) {
 	cordova.exec(successCallback, errorCallback, "PinterestPlugin", "pin", [sourceURL, imageURL, description]);
-};
-PinterestPlugin.prototype.canPinWithSDK = function(successCallback, errorCallback) {
-	cordova.exec(successCallback, errorCallback, "PinterestPlugin", "canPinWithSDK", []);
 };
 module.exports = new PinterestPlugin();
